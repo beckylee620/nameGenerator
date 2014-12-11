25cb4539474d5f3e59b1ed5363a99d20345ea00d
Becky <msbeckylee@gmail.com>
2014-12-11 17:04:18 +0000

    Update and rename nameGenerator.js to chooseProbly.js

---
+++ chooseProbly.js
@@ -0,0 +1,29 @@
+<script type="text/javascript">
+
+function chooseProbly() {
+  for (possibleTo in sampleDataX) {
+    rangeCount += sampleDataX[possibleTo];
+    property = possibleTo;
+    toRange[property] = rangeCount;
+  }
+  
+  randomNumber = Math.random();
+  
+  for (possibilities in toRange) {
+    if (Number(toRange[possibilities]) - randomNumber >= 0) {
+      if (chosenTo == "") {
+        chosenTo = possibilities;
+        difference = Number(toRange[possibilities]) - randomNumber;
+      } else if (Number(toRange[possibilities]) - randomNumber < difference) {
+        chosenTo = possibilities;
+        difference = Number(toRange[possibilities]) - randomNumber;
+      }
+    }
+  }
+  
+  difference = 1;
+  toRange = {};
+  rangeCount = 0;
+}
+
+</script>
